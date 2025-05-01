8f0a6276819d8c585af44f60bfbc2a936d3b4afe
lexvvcy <alexcihc@gmail.com>
2025-05-01 14:42:45 +0800

    A

--- code/GSIM_pipeline.v
+++ code/GSIM_pipeline.v
@@ -15,14 +15,14 @@ module GSIM(clk, reset, in_en, b_in, out_valid, x_out);
     reg signed [15:0] b            [0:15];   //store offsets b1 b2... b16 16bits each
     reg signed [31:0] ans          [0:15];   //store answers x1 x2... x16 32bits each
 
-    reg signed [40:0] pipeline_r   [0:5 ];
-    reg signed [40:0] pipeline_w   [0:5 ];
-    reg signed [40:0] pipeline_src [0:5 ];
+    reg signed [33:0] pipeline_r   [0:5 ];
+    reg signed [33:0] pipeline_w   [0:5 ];
+    reg signed [33:0] pipeline_src [0:5 ];
 
-    reg signed [40:0] pipeline_support_1;
-    reg signed [40:0] pipeline_support_2;
-    reg signed [40:0] pipeline_support_3;
-    reg        [12:0] cnt_r, cnt_w;
+    reg signed [33:0] pipeline_support_1;
+    reg signed [33:0] pipeline_support_2;
+    reg signed [33:0] pipeline_support_3;
+    reg        [11:0] cnt_r, cnt_w;
     reg        [ 3:0] mapping;
 
     wire [3:0] idx0 = (cnt_r[3] | cnt_r[2]) ? 4'd13 : 4'd12;
@@ -32,41 +32,41 @@ module GSIM(clk, reset, in_en, b_in, out_valid, x_out);
     wire [3:0] idx4 = (cnt_r[3] & cnt_r[2]) ? 4'd5 : 4'd4;
     wire [3:0] idx5 = (cnt_r[3] | cnt_r[2]) ? 4'd12 : 4'd11;
 
-    localparam MAX_ITER     = 200; //maximum number of iterations
+    localparam MAX_ITER     = 70; //maximum number of iterations
     localparam PIPELINE_MAX = (16 * MAX_ITER) - 1;
 
     assign out_valid = (state_r == SEND) ? 1 : 0; //output valid when in SEND state
     assign x_out = ans[mapping];
 
-    function signed [40:0] mul_3;
-        input signed [31:0] a;
+    function signed [33:0] mul_3_2;
+        input signed [33:0] a;
         begin
-            mul_3 = a + (a<<<1);
+            mul_3_2 = (a>>>2) + (a>>>1);
         end
     endfunction
 
-    function signed [40:0] mul_18;
-        input signed [31:0] a;
+    function signed [33:0] mul_18_2;
+        input signed [33:0] a;
         begin
-            mul_18 = (a<<<4) + (a<<<1);
+            mul_18_2 = (a<<<2) + (a>>>1);
         end
     endfunction
 
-    function signed [40:0] mul_39;
-        input signed [31:0] a;
+    function signed [33:0] mul_39_2;
+        input signed [33:0] a;
         begin
-            mul_39 = (a<<<5)  + (a<<<2)  + (a<<<1)  + a;
+            mul_39_2 = (a<<<3)  + a  + (a>>>1)  + (a>>>2);
         end
     endfunction
 
     //pipeline choose
     always @(*) begin
-        pipeline_src[0] = {{9{ans[idx0][31]}}, ans[idx0]};  // future 3
-        pipeline_src[1] = {{9{ans[idx1][31]}}, ans[idx1]};  // past 3
-        pipeline_src[2] = {{9{ans[idx2][31]}}, ans[idx2]};  // future 2
-        pipeline_src[3] = {{9{ans[idx3][31]}}, ans[idx3]};  // past 2
-        pipeline_src[4] = {{9{ans[idx4][31]}}, ans[idx4]};  // future 1
-        pipeline_src[5] = {{9{ans[idx5][31]}}, ans[idx5]};  // past 1
+        pipeline_src[0] = {ans[idx0],2'sd0};  // future 3
+        pipeline_src[1] = {ans[idx1],2'sd0};  // past 3
+        pipeline_src[2] = {ans[idx2],2'sd0};  // future 2
+        pipeline_src[3] = {ans[idx3],2'sd0};  // past 2
+        pipeline_src[4] = {ans[idx4],2'sd0};  // future 1
+        pipeline_src[5] = {ans[idx5],2'sd0};  // past 1
 
         case (cnt_r[3:0])  
             0: begin
@@ -99,40 +99,20 @@ module GSIM(clk, reset, in_en, b_in, out_valid, x_out);
     //pipeline count
     always @(*) begin
 
-        pipeline_w[0] = mul_3({b[mapping], 16'd0});
-        pipeline_w[1] = mul_3(pipeline_src[0] + pipeline_src[1]);
-        pipeline_w[2] = mul_18(pipeline_src[2] + pipeline_src[3]);
-        pipeline_w[3] = mul_39(pipeline_src[4] + pipeline_src[5]);
+        pipeline_w[0] = mul_3_2({b[mapping],18'sd0});
+        pipeline_w[1] = mul_3_2(pipeline_src[0] + pipeline_src[1]);
+        pipeline_w[2] = mul_18_2(pipeline_src[2] + pipeline_src[3]);
+        pipeline_w[3] = mul_39_2(pipeline_src[4] + pipeline_src[5]);
 
-        pipeline_support_1 = (pipeline_r[0] - pipeline_r[2]) + (pipeline_r[1] + pipeline_r[3]);
+        pipeline_support_1 = ((pipeline_r[0] - pipeline_r[2]) >>>2) + ((pipeline_r[1] + pipeline_r[3]) >>>2);
         pipeline_w[4] = pipeline_support_1 + (pipeline_support_1 >>> 4);
 
         pipeline_w[5] = pipeline_r[4] + (pipeline_r[4] >>> 8);
 
-        pipeline_support_2 = pipeline_r[5] + (pipeline_r[5] >>> 12);
-        pipeline_support_3 = pipeline_support_2 >>> 6;
+        pipeline_support_2 = pipeline_r[5] + (pipeline_r[5] >>> 16);
+        pipeline_support_3 = pipeline_support_2 >>> 2;
 
     end
-
-    /*
-    // check architecture correct?
-    always @(*) begin
-
-        pipeline_w[0] = 3*{b[mapping], 16'd0};
-        pipeline_w[1] = 3*(pipeline_src[0] + pipeline_src[1]);
-        pipeline_w[2] = 18*(pipeline_src[2] + pipeline_src[3]);
-        pipeline_w[3] = 39*(pipeline_src[4] + pipeline_src[5]);
-
-        pipeline_support_1 = (pipeline_r[0] - pipeline_r[2]) + (pipeline_r[1] + pipeline_r[3]);
-        pipeline_w[4] = (pipeline_support_1)*(1+2**(-4));
-
-        pipeline_w[5] = (pipeline_r[4])*(1+2**(-8));
-
-        pipeline_support_2 = (pipeline_r[5])*(1+2**(-12));
-        pipeline_support_3 = (pipeline_support_2)/64;
-
-    end
-    */
 
     //FSM
     always @(*) begin
@@ -176,22 +156,22 @@ module GSIM(clk, reset, in_en, b_in, out_valid, x_out);
 
     always @(*) begin
         case (cnt_r[3:0])
-            0:  mapping = 4'd0;
-            1:  mapping = 4'd4;
-            2:  mapping = 4'd8;
-            3:  mapping = 4'd12;
-            4:  mapping = 4'd1;
-            5:  mapping = 4'd5;
-            6:  mapping = 4'd9;
-            7:  mapping = 4'd13;
-            8:  mapping = 4'd2;
-            9:  mapping = 4'd6;
-            10: mapping = 4'd10;
-            11: mapping = 4'd14;
-            12: mapping = 4'd3;
-            13: mapping = 4'd7;
-            14: mapping = 4'd11;
-            15: mapping = 4'd15;
+            4'd0:  mapping = 4'd0;
+            4'd1:  mapping = 4'd4;
+            4'd2:  mapping = 4'd8;
+            4'd3:  mapping = 4'd12;
+            4'd4:  mapping = 4'd1;
+            4'd5:  mapping = 4'd5;
+            4'd6:  mapping = 4'd9;
+            4'd7:  mapping = 4'd13;
+            4'd8:  mapping = 4'd2;
+            4'd9:  mapping = 4'd6;
+            4'd10: mapping = 4'd10;
+            4'd11: mapping = 4'd14;
+            4'd12: mapping = 4'd3;
+            4'd13: mapping = 4'd7;
+            4'd14: mapping = 4'd11;
+            4'd15: mapping = 4'd15;
         endcase
     end
 
@@ -217,7 +197,7 @@ module GSIM(clk, reset, in_en, b_in, out_valid, x_out);
         end
         else begin
             if (state_r == RECEIVE && in_en) begin
-                b[mapping] <= b_in;
+                b[cnt_r] <= b_in;
             end
         end
     end
@@ -278,7 +258,7 @@ module GSIM(clk, reset, in_en, b_in, out_valid, x_out);
                 ans[9]  <= ans[10];
                 ans[10] <= ans[11];
                 ans[11] <= ans[12];
-                ans[12] <= pipeline_support_3[31:0];
+                ans[12] <= pipeline_support_3[33:2];
                 ans[13] <= ans[14];
                 ans[14] <= ans[15];
                 ans[15] <= ans[0];
